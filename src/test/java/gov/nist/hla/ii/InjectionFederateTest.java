3912d70713a5fa54163b52bb5eb587953731fc19
Roberts, Geoffry C <geoffry.roberts@nist.gov>
2017-06-05 08:09:02 -0400

    Passed basic tests. Has not been tested a part of a running federation.

--- src/test/java/gov/nist/hla/ii/InjectionFederateTest.java
+++ src/test/java/gov/nist/hla/ii/InjectionFederateTest.java
@@ -13,6 +13,7 @@ import org.eclipse.emf.ecore.EObject;
 import org.ieee.standards.ieee1516._2010.InteractionClassType;
 import org.ieee.standards.ieee1516._2010.InteractionClassType1;
 import org.ieee.standards.ieee1516._2010.InteractionsType;
+import org.ieee.standards.ieee1516._2010.ObjectClassType;
 import org.ieee.standards.ieee1516._2010.ObjectModelType;
 import org.ieee.standards.ieee1516._2010._2010Factory;
 import org.ieee.standards.ieee1516._2010._2010Package;
@@ -73,10 +74,10 @@ public class InjectionFederateTest {
 	public void testGetInteractionSubscribe() {
 		try {
 			sut.loadConfiguration(CONFIG_FILE);
-			Set<String> set = sut.getInteractionSubscribe();
+			Set<InteractionClassType> set = sut.getInteractionSubscribe();
 			assertEquals(13, set.size());
-			for (String s : set) {
-				System.out.println(s);
+			for (InteractionClassType ict : set) {
+				System.out.println(ict.getName().getValue());
 			}
 		} catch (IOException | PropertyNotFound | PropertyNotAssigned e) {
 			e.printStackTrace();
@@ -87,43 +88,41 @@ public class InjectionFederateTest {
 	public void testGetInteractionPublish() {
 		try {
 			sut.loadConfiguration(CONFIG_FILE);
-			Set<String> set = sut.getInteractionPublish();
+			Set<InteractionClassType> set = sut.getInteractionPublish();
 			assertEquals(16, set.size());
-			for (String s : set) {
-				System.out.println(s);
+			for (InteractionClassType ict : set) {
+				System.out.println(ict.getName().getValue());
 			}
 		} catch (IOException | PropertyNotFound | PropertyNotAssigned e) {
 			e.printStackTrace();
 		}
 	}
-	// @Test
-	public void testDe() {
-		Deserialize.getResourceSet().getResourceFactoryRegistry().getExtensionToFactoryMap().put("_2010",
-				new _2010ResourceFactoryImpl());
-		Deserialize.getResourceSet().getPackageRegistry().put(_2010Package.eNS_URI, _2010Package.eINSTANCE);
-		EObject eObject = Deserialize.it("fom/TripleFederation._2010");
+	
+	@Test
+	public void testGetObjectSubscribe() {
+		try {
+			sut.loadConfiguration(CONFIG_FILE);
+			Set<ObjectClassType> set = sut.getObjectSubscribe();
+			assertEquals(2, set.size());
+			for (ObjectClassType oct : set) {
+				System.out.println(oct.getName().getValue());
+			}
+		} catch (IOException | PropertyNotFound | PropertyNotAssigned e) {
+			e.printStackTrace();
+		}
 	}
-
-	// @Test
-	public void testSe() {
-		Serialize se = new Serialize();
-		ObjectModelType objectModel = create();
-		String s = Serialize.it(objectModel, "fom/TripleFederation1.xml");
-
-	}
-
-	private ObjectModelType create() {
-		ObjectModelType objectModel = _2010Factory.eINSTANCE.createObjectModelType();
-		InteractionsType intr = _2010Factory.eINSTANCE.createInteractionsType();
-		InteractionClassType1 intrClassType1 = _2010Factory.eINSTANCE.createInteractionClassType1();
-		intr.setInteractionClass(intrClassType1);
-		InteractionClassType1 intr1ClassType = _2010Factory.eINSTANCE.createInteractionClassType1();
-		intrClassType1.getInteractionClass().add(intr1ClassType);
-		InteractionClassType1 intr2ClassType = _2010Factory.eINSTANCE.createInteractionClassType1();
-		intrClassType1.getInteractionClass().add(intr2ClassType);
-		InteractionClassType1 intr3ClassType = _2010Factory.eINSTANCE.createInteractionClassType1();
-		intrClassType1.getInteractionClass().add(intr3ClassType);
-		objectModel.setInteractions(intr);
-		return objectModel;
+	
+	@Test
+	public void testGetObjectPublish() {
+		try {
+			sut.loadConfiguration(CONFIG_FILE);
+			Set<ObjectClassType> set = sut.getObjectPublish();
+			assertEquals(2, set.size());
+			for (ObjectClassType oct : set) {
+				System.out.println(oct.getName().getValue());
+			}
+		} catch (IOException | PropertyNotFound | PropertyNotAssigned e) {
+			e.printStackTrace();
+		}
 	}
 }
